6b785c707c8ab3001416cf84f7a8da363f404400
Sara Rahman <68961518+Sara-Rahman@users.noreply.github.com>
2021-11-04 16:36:14 +0600

    #

---
+++ resources/views/admin/fixed/footer.blade.php
@@ -0,0 +1,17 @@
+<footer class="footer">
+  <div class="container footer--flex">
+    <div class="footer-start">
+
+    
+      <!-- <p>2021 © Radiant Foundation Dashboard >elegant-dashboard.com</a></p><br>
+          <p>Shahida Rahman</p> -->
+    </div>
+    <ul class="footer-end">
+      <li>Shahida Rahman</li>
+      <li>Radiant Foundation</li>
+      
+    </ul>
+  </div>
+</footer>
+  <!-- </div>
+</div> -->
